abe3dcad87fb6d679a3cf1f3bb2d2ecd44d82b66
Ramakrishna G. Kannari <rkannari@BLR.ALLEGISINDIA.COM>
2023-02-24 09:43:43 +0530

    Car

---
+++ javafsddemo/src/javafsddemo/Car.java
@@ -0,0 +1,13 @@
+package javafsddemo;
+
+public class Car {
+	public String color;
+	public static String manufcturer;
+	public int noOfWheels;
+	
+	public Car() {
+		color="White";
+		noOfWheels=4;
+		manufcturer = "Benz";
+	}
+}
--- javafsddemo/src/javafsddemo/MyFirstClass.java
+++ javafsddemo/src/javafsddemo/MyFirstClass.java
@@ -3,6 +3,10 @@ package javafsddemo;
 public class MyFirstClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		Car mycar = new Car();
+		System.out.println(mycar.noOfWheels);
+		System.out.println(Car.manufcturer);
 	}
 
 }
